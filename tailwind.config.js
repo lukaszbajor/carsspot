d975f89340c6bc62015bf28f9e331cc6d4bd33bb
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2024-09-09 17:59:11 +0200

    nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./dist/index.html"],
+	theme: {
+		extend: {
+			container: {
+				center: "true",
+			},
+			colors: {
+				"custom-white": "#F7F7F7",
+				"custom-blue": "#0147FF",
+			},
+			fontFamily: {
+				"roboto-condensed": ["Roboto Condensed", "sans-serif"],
+				"roboto-flex": ["Roboto Flex", "sans-serif"],
+				"bebas-neue": ["Bebas Neue", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};
